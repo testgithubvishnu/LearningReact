15d138942f85b860b177d4e5e1b2162f6bacec33
Manale Akshata Kamlakar <akshatamanale@gmail.com>
2024-02-14 22:12:06 +0530

    today's task

--- CallingAPI/src/Products.jsx
+++ CallingAPI/src/Products.jsx
@@ -15,10 +15,10 @@ function Products() {
   }, []);
 
   return (
-    <div className="products">
-      {products.map((product) => {
+    <div className="products" key={product.id}>
+      {products.map((product, index) => {
         return (
-          <div className="product">
+          <div className="product" key={index}>
             <img className="p-image" src={product.image} alt="" />
             <h2 className="p-title">{product.title}</h2>
             <h2>{product.price}</h2>
